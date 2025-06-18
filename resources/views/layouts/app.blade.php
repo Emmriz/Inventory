72349eb4e539753a07b43b797c35ce684d53aa26
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2025-06-18 18:55:16 +0100

    Update the header title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventory Pro - @yield('title', 'Dashboard')</title>
+    <title>GGCCHQ INVENTORY - @yield('title', 'Dashboard')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
